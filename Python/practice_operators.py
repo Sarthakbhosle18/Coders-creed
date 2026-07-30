3e42f71e46c1de413a6c2185ac0316d043bc51c8
Sarthak Bhosale <154578679+Sarthakbhosle18@users.noreply.github.com>
2026-07-30 13:44:26 +0530

    Add files via upload

---
+++ Python/practice_operators.py
@@ -0,0 +1,16 @@
+num1 = 13
+num2 = 10
+
+# num3 = num1 + num2
+# num3 = num1 - 2
+# num3 = num1 *num2
+# num3 = num1
+# num1 = num3 - num2
+# print(num1)
+
+# num1 +=4 
+# num2 -=6
+# print (num1)
+# print(num2)
+
+print ("And",num1>num2 and num2 == 5)
